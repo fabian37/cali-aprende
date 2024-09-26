f4fba73bd95c3a2abad3d5dc4ede5946d11e26db
fabian37 <fabian37gomez@gmail.com>
2024-09-25 20:03:38 -0500

    feat: add obj tree

--- src/pages/ObjectivesTree.tsx
+++ src/pages/ObjectivesTree.tsx
@@ -1,12 +1,14 @@
 import "./StakeholderAnalysis.css";
-import { Tools } from "./Tools";
+import obj from "../../public/obj-tree.png";
 
 export function ObjectivesTree() {
   return (
     <main className="app-container">
       <section>
         <h1>Árbol de objetivos</h1>
-        <Tools />
+        <div className="about__card">
+          <img src={obj} alt="Árbol de objetivos" />
+        </div>
       </section>
     </main>
   );
